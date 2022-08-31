cfd8845cb19690468e80446b83b831e5d34c70ba
yinweli <yinweli@gmail.com>
2022-08-31 15:54:30 +0800

    [feature] 新增錯誤編號類型

--- external/mizugo/errors.go
+++ external/mizugo/errors.go
@@ -9,46 +9,52 @@ package mizugo
 //
 //         return nil
 //     }
-// 當需要取出錯誤編號時(例如要回傳給客戶端時), 呼叫mizugo.UnwrapErrorID來取得錯誤
+// 當需要取得錯誤編號時(例如要回傳給客戶端時), 呼叫mizugo.UnwrapErrorID來取得錯誤
+// 當需要取得錯誤訊息時, 仍然可以用error.Error取得, 不過錯誤訊息不會出現錯誤編號
 // 例如:
 //     errID := UnwrapErrorID(err)
 
+// ErrID 錯誤編號類型
+type ErrID = int64
+
+const UnknownErrID ErrID = -1 // 不明的錯誤編號
+
 // Errorf 產生錯誤
-func Errorf(errID int64, err error) error {
-    return &wrapError{
-        errID: errID,
-        err:   err,
-    }
+func Errorf(errID ErrID, err error) error {
+	return &wrapError{
+		errID: errID,
+		err:   err,
+	}
 }
 
-// UnwrapErrID 取出錯誤編號, 如果錯誤並非wrapError, 則會獲得0
-func UnwrapErrID(err error) int64 {
-    if u, ok := err.(interface {
-        ErrID() int64
-    }); ok {
-        return u.ErrID()
-    } // if
+// UnwrapErrID 取得錯誤編號, 如果錯誤並非wrapError, 則會獲得UnknownErrID
+func UnwrapErrID(err error) ErrID {
+	if u, ok := err.(interface {
+		ErrID() ErrID
+	}); ok {
+		return u.ErrID()
+	} // if
 
-    return 0
+	return UnknownErrID
 }
 
 // wrapError 錯誤結構
 type wrapError struct {
-    errID int64 // 錯誤編號
-    err   error // 錯誤物件
+	errID ErrID // 錯誤編號
+	err   error // 錯誤物件
 }
 
 // ErrID 取得錯誤編號
-func (this *wrapError) ErrID() int64 {
-    return this.errID
+func (this *wrapError) ErrID() ErrID {
+	return this.errID
 }
 
 // Unwrap 取得錯誤物件
 func (this *wrapError) Unwrap() error {
-    return this.err
+	return this.err
 }
 
 // wrapError 取得錯誤訊息
 func (this *wrapError) Error() string {
-    return this.err.Error()
+	return this.err.Error()
 }
