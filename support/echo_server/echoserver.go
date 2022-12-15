b7cd851c7f06b79035cf4aaec6dcab4c3f83b34f
yinweli <yinweli@gmail.com>
2022-12-15 18:00:23 +0800

    [feature] 移除不需要的程式碼

--- support/echo_server/echoserver.go
+++ support/echo_server/echoserver.go
@@ -2,18 +2,4 @@ package main
 
 func main() {
 	// 這裡會啟動一個使用字串以及md5編碼作為封包內容的回音伺服器
-	/*
-		mizugos.Initialize(&logs.ZapLogger{
-			Name:       "echo",
-			Path:       "logs",
-			Json:       false,
-			Console:    true,
-			File:       true,
-			Level:      logs.LevelDebug,
-			MaxSize:    100,
-			MaxTime:    0,
-			MaxBackups: 0,
-			Compress:   false,
-		})
-	*/
 }
