384c52930db9bd9b7b47400a40ce1f620c8d16a6
yinweli <yinweli@gmail.com>
2022-12-15 17:41:54 +0800

    [feature] fix race condition

--- mizugos/mizugo.go
+++ mizugos/mizugo.go
@@ -2,6 +2,7 @@ package mizugos
 
 import (
 	"fmt"
+	"sync"
 	"sync/atomic"
 
 	"github.com/yinweli/Mizugo/cores/entitys"
@@ -18,13 +19,11 @@ type Finalize func()
 
 // Start 啟動伺服器, 為了讓程式持續執行, 此函式不能用執行緒執行
 func Start(name string, logger logs.Logger, initialize Initialize, finalize Finalize) {
-	if serv.exec.CompareAndSwap(false, true) == false {
+	if serv.init.CompareAndSwap(false, true) == false {
 		return
 	} // if
 
 	fmt.Printf("%v initialize\n", name)
-
-	serv.close = make(chan bool, 1)
 	serv.netmgr = nets.NewNetmgr()
 	serv.entitymgr = entitys.NewEntitymgr()
 	serv.tagmgr = tags.NewTagmgr()
@@ -40,77 +39,100 @@ func Start(name string, logger logs.Logger, initialize Initialize, finalize Fina
 		goto Finish
 	} // if
 
-	Info(name).Message("server start").End()
+	fmt.Printf("%v start\n", name)
+	serv.exec.Store(true)
+	serv.close.Add(1)
+	serv.close.Wait() // 進行等待, 直到關閉伺服器
+	goto Shutdown
 
-	// 進行無限迴圈, 直到關閉伺服器
-	for {
-		select {
-		case <-serv.close:
-			goto Shutdown
-		} // select
-	} // for
-
-Shutdown:
+Shutdown: // 關閉伺服器處理
 	finalize()
 	serv.logger.Finalize()
 	fmt.Printf("%v shutdown\n", name)
 
-Finish:
-	close(serv.close)
+Finish: // 結束處理
 	serv.netmgr = nil
 	serv.entitymgr = nil
 	serv.tagmgr = nil
 	serv.logger = nil
+	serv.init.Store(false)
 	serv.exec.Store(false)
 	fmt.Printf("%v finish\n", name)
 }
 
 // Close 關閉伺服器
 func Close() {
-	if serv.exec.Load() {
-		serv.close <- true
-	} // if
+	serv.close.Done()
 }
 
 // Netmgr 取得網路管理器
 func Netmgr() *nets.Netmgr {
-	return serv.netmgr
+	if serv.exec.Load() {
+		return serv.netmgr
+	} // if
+
+	return nil
 }
 
 // Entitymgr 實體管理器
 func Entitymgr() *entitys.Entitymgr {
-	return serv.entitymgr
+	if serv.exec.Load() {
+		return serv.entitymgr
+	} // if
+
+	return nil
 }
 
 // Tagmgr 實體管理器
 func Tagmgr() *tags.Tagmgr {
-	return serv.tagmgr
+	if serv.exec.Load() {
+		return serv.tagmgr
+	} // if
+
+	return nil
 }
 
 // Debug 記錄除錯訊息
 func Debug(label string) logs.Stream {
-	return serv.logger.New(label, logs.LevelDebug)
+	if serv.exec.Load() {
+		return serv.logger.New(label, logs.LevelDebug)
+	} // if
+
+	return nil
 }
 
 // Info 記錄一般訊息
 func Info(label string) logs.Stream {
-	return serv.logger.New(label, logs.LevelInfo)
+	if serv.exec.Load() {
+		return serv.logger.New(label, logs.LevelInfo)
+	} // if
+
+	return nil
 }
 
 // Warn 記錄警告訊息
 func Warn(label string) logs.Stream {
-	return serv.logger.New(label, logs.LevelWarn)
+	if serv.exec.Load() {
+		return serv.logger.New(label, logs.LevelWarn)
+	} // if
+
+	return nil
 }
 
 // Error 記錄錯誤訊息
 func Error(label string) logs.Stream {
-	return serv.logger.New(label, logs.LevelError)
+	if serv.exec.Load() {
+		return serv.logger.New(label, logs.LevelError)
+	} // if
+
+	return nil
 }
 
 // serv 伺服器資料
 var serv struct {
+	init      atomic.Bool        // 初始化旗標
 	exec      atomic.Bool        // 執行旗標
-	close     chan bool          // 關閉通道
+	close     sync.WaitGroup     // 關閉旗標
 	netmgr    *nets.Netmgr       // 網路管理器
 	entitymgr *entitys.Entitymgr // 實體管理器
 	tagmgr    *tags.Tagmgr       // 標籤管理器
--- mizugos/mizugo_test.go
+++ mizugos/mizugo_test.go
@@ -1,11 +1,16 @@
 package mizugos
 
 import (
+	"fmt"
+	"sync/atomic"
 	"testing"
+	"time"
 
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/suite"
 	"go.uber.org/goleak"
 
+	"github.com/yinweli/Mizugo/cores/logs"
 	"github.com/yinweli/Mizugo/testdata"
 )
 
@@ -19,7 +24,7 @@ type SuiteMizugo struct {
 }
 
 func (this *SuiteMizugo) SetupSuite() {
-	this.Change("test-mizugo")
+	this.Change("test-mizugos-mizugo")
 }
 
 func (this *SuiteMizugo) TearDownSuite() {
@@ -30,12 +35,76 @@ func (this *SuiteMizugo) TearDownTest() {
 	goleak.VerifyNone(this.T())
 }
 
-func (this *SuiteMizugo) TestStart() {
-	/*
-		Initialize(&logs.EmptyLogger{})
-		assert.NotNil(this.T(), Logger())
-		assert.NotNil(this.T(), Netmgr())
-		assert.NotNil(this.T(), Entitymgr())
-		assert.NotNil(this.T(), Tagmgr())
-	*/
+func (this *SuiteMizugo) TestMizugo() {
+	name := "mizugo"
+	validInitialize := atomic.Bool{}
+	validFinalize := atomic.Bool{}
+
+	assert.Nil(this.T(), Netmgr())
+	assert.Nil(this.T(), Entitymgr())
+	assert.Nil(this.T(), Tagmgr())
+	assert.Nil(this.T(), Debug(name))
+	assert.Nil(this.T(), Info(name))
+	assert.Nil(this.T(), Warn(name))
+	assert.Nil(this.T(), Error(name))
+
+	go Start(name, &logs.EmptyLogger{},
+		func() error {
+			validInitialize.Store(true)
+			return nil
+		},
+		func() {
+			validFinalize.Store(true)
+		})
+
+	time.Sleep(time.Second)
+	assert.NotNil(this.T(), Netmgr())
+	assert.NotNil(this.T(), Entitymgr())
+	assert.NotNil(this.T(), Tagmgr())
+	assert.NotNil(this.T(), Debug(name))
+	assert.NotNil(this.T(), Info(name))
+	assert.NotNil(this.T(), Warn(name))
+	assert.NotNil(this.T(), Error(name))
+
+	go Close()
+
+	time.Sleep(time.Second)
+	assert.True(this.T(), validInitialize.Load())
+	assert.True(this.T(), validFinalize.Load())
+}
+
+func (this *SuiteMizugo) TestTwice() {
+	name := "twice"
+	valid := atomic.Int64{}
+
+	go Start(name, &logs.EmptyLogger{},
+		func() error {
+			valid.Add(1)
+			return nil
+		},
+		func() {
+		})
+	go Start(name, &logs.EmptyLogger{},
+		func() error {
+			valid.Add(1)
+			return nil
+		},
+		func() {
+		})
+
+	time.Sleep(time.Second)
+
+	go Close()
+
+	time.Sleep(time.Second)
+	assert.Equal(this.T(), int64(1), valid.Load())
+}
+
+func (this *SuiteMizugo) TestFailed() {
+	go Start("failed", &logs.EmptyLogger{},
+		func() error {
+			return fmt.Errorf("failed")
+		},
+		func() {
+		})
 }
