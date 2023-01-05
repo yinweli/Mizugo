57ecdc4a615aa548926c5cdce68e42414fc586c8
yinweli <yinweli@gmail.com>
2023-01-05 17:47:25 +0800

    [feature] 更新效能測試

--- mizugos/utils/encoding_test.go
+++ mizugos/utils/encoding_test.go
@@ -47,24 +47,72 @@ func (this *SuiteEncoding) TestBase64() {
 	assert.Equal(this.T(), input, output)
 }
 
-func BenchmarkMD5String(b *testing.B) {
-	input := []byte("benchmark md5 string")
+func BenchmarkMD5String1024(b *testing.B) {
+	input := []byte(RandString(1024))
 
 	for i := 0; i < b.N; i++ {
 		_ = MD5String(input)
 	} // for
 }
 
-func BenchmarkBase64Encode(b *testing.B) {
-	input := []byte("benchmark base64 encode string")
+func BenchmarkMD5String2048(b *testing.B) {
+	input := []byte(RandString(2048))
+
+	for i := 0; i < b.N; i++ {
+		_ = MD5String(input)
+	} // for
+}
+
+func BenchmarkMD5String4096(b *testing.B) {
+	input := []byte(RandString(4096))
+
+	for i := 0; i < b.N; i++ {
+		_ = MD5String(input)
+	} // for
+}
+
+func BenchmarkBase64Encode1024(b *testing.B) {
+	input := []byte(RandString(1024))
 
 	for i := 0; i < b.N; i++ {
 		_ = Base64Encode(input)
 	} // for
 }
 
-func BenchmarkBase64Decode(b *testing.B) {
-	input := []byte("benchmark base64 decode string")
+func BenchmarkBase64Encode2048(b *testing.B) {
+	input := []byte(RandString(2048))
+
+	for i := 0; i < b.N; i++ {
+		_ = Base64Encode(input)
+	} // for
+}
+
+func BenchmarkBase64Encode4096(b *testing.B) {
+	input := []byte(RandString(4096))
+
+	for i := 0; i < b.N; i++ {
+		_ = Base64Encode(input)
+	} // for
+}
+
+func BenchmarkBase64Decode1024(b *testing.B) {
+	input := []byte(RandString(1024))
+
+	for i := 0; i < b.N; i++ {
+		_, _ = Base64Decode(input)
+	} // for
+}
+
+func BenchmarkBase64Decode2048(b *testing.B) {
+	input := []byte(RandString(2048))
+
+	for i := 0; i < b.N; i++ {
+		_, _ = Base64Decode(input)
+	} // for
+}
+
+func BenchmarkBase64Decode4096(b *testing.B) {
+	input := []byte(RandString(4096))
 
 	for i := 0; i < b.N; i++ {
 		_, _ = Base64Decode(input)
