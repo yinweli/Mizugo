8d5d3f46c5ee1bb2c8ac6a7a08d5dd095740c48d
yinweli <yinweli@gmail.com>
2023-01-16 18:30:17 +0800

    [feature] 重整訊息proto以及產生proto的批次檔

--- support/proto/example_message/example_message.bat
+++ support/proto/example_message/example_message.bat
@@ -1,6 +1,13 @@
-protoc --go_out=. messageid.proto
-protoc --go_out=. msgkey.proto
-protoc --go_out=. msgping.proto
+rm -r .\messages\
+
+protoc --go_out=. msgid.proto
+protoc --go_out=. msgverify.proto
+
+rm -r ..\..\example_server\internal\messages\
+rm -r ..\..\example_clientgo\internal\messages\
+
+mkdir ..\..\example_server\internal\messages\
+mkdir ..\..\example_clientgo\internal\messages\
 
 copy .\messages\*.* ..\..\example_server\internal\messages\
 copy .\messages\*.* ..\..\example_clientgo\internal\messages\
