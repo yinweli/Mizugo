caf3f1a62c692c6c7832b560a1638ed09f887cc1
yinweli <yinweli@gmail.com>
2023-01-26 16:33:43 +0800

    [feature] 新增client-unity專案

---
+++ Runtime/define.cs
@@ -0,0 +1,36 @@
+﻿namespace Mizugo
+{
+    /// <summary>
+    /// 位址資料
+    /// </summary>
+    internal struct Addr
+    {
+        /// <summary>
+        /// 位址
+        /// </summary>
+        public string ip;
+
+        /// <summary>
+        /// 埠號
+        /// </summary>
+        public int port;
+    }
+
+    /// <summary>
+    /// 日誌介面
+    /// </summary>
+    internal interface Logger
+    {
+        /// <summary>
+        /// 記錄一般訊息
+        /// </summary>
+        /// <param name="message">訊息字串</param>
+        public void Info(string message);
+
+        /// <summary>
+        /// 記錄錯誤訊息
+        /// </summary>
+        /// <param name="message">訊息字串</param>
+        public void Error(string message);
+    }
+}
