ea1e45ec22eda3df96a10c808f7fae7bc551c52a
yinweli <yinweli@gmail.com>
2023-01-31 18:02:44 +0800

    [feature] 新增測試封包到client-unity

---
+++ Runtime/procs/json.cs
@@ -0,0 +1,23 @@
+namespace Mizugo
+{
+    /// <summary>
+    /// json處理器
+    /// </summary>
+    public class Json : Procmgr
+    {
+        public override byte[] Encode(object input)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override object Decode(byte[] input)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override bool Process(object message)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
