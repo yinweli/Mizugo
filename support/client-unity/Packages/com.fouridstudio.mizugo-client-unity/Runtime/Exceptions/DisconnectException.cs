e23067972cd2652a7314902f5a4e05dbc3a691e6
yinweli <yinweli@gmail.com>
2023-08-25 16:26:54 +0800

    Fix | 修正斷線時不再拋出異常, 只會剩下斷線事件

---
+++ support/client-unity/Packages/com.fouridstudio.mizugo-client-unity/Runtime/Exceptions/DisconnectException.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Mizugo
+{
+    /// <summary>
+    /// 中斷連線異常
+    /// </summary>
+    public class DisconnectException : Exception
+    {
+        public DisconnectException()
+            : base("disconnect") { }
+    }
+}
