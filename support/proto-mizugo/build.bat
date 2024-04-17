9a93be2e3f1b95cbe2a9f5f007760674279a7a4e
yinweli <yinweli@gmail.com>
2024-04-17 19:47:45 +0800

    Feature | 新增raven訊息

--- support/proto-mizugo/build.bat
+++ support/proto-mizugo/build.bat
@@ -3,10 +3,11 @@ echo off
 REM Generate the message for GO
 echo #### Generate the message for GO
 set source=msg-go
-set target=..\..\..\mizugos\msgs
+set target=..\..\mizugos\msgs
 
 rm -r %source%\msgs
 protoc --go_out=%source% protomsg.proto
+protoc --go_out=%source% raven.proto
 
 rm -r %target%
 mkdir %target%
@@ -16,11 +17,12 @@ copy %source%\msgs-json\*.* %target%
 REM Generate the message for Unity
 echo #### Generate the message for Unity
 set source=msg-cs
-set target=..\..\..\support\client-unity\Packages\com.fouridstudio.mizugo-client-unity\Runtime\Msgs
+set target=..\..\support\client-unity\Packages\com.fouridstudio.mizugo-client-unity\Runtime\Msgs
 
 rm -r %source%\msgs
 mkdir %source%\msgs
 protoc --csharp_out=%source%\msgs protomsg.proto
+protoc --csharp_out=%source%\msgs raven.proto
 
 rm -r %target%
 mkdir %target%
--- support/proto-test/build.bat
+++ support/proto-test/build.bat
@@ -3,8 +3,8 @@ echo off
 REM Generate the message for GO
 echo #### Generate the message for GO
 set source=msg-go
-set targets=..\..\..\support\test-server\msgs
-set targetc=..\..\..\support\test-client-go\msgs
+set targets=..\..\support\test-server\msgs
+set targetc=..\..\support\test-client-go\msgs
 
 rm -r %source%\msgs
 protoc --go_out=%source% msgid.proto
@@ -23,7 +23,7 @@ copy %source%\msgs-json\*.* %targetc%
 REM Generate the message for Unity
 echo #### Generate the message for Unity
 set source=msg-cs
-set targetc=..\..\..\support\test-client-cs\Assets\Scripts\Msgs
+set targetc=..\..\support\test-client-cs\Assets\Scripts\Msgs
 
 rm -r %source%\msgs
 mkdir %source%\msgs
@@ -38,7 +38,7 @@ copy %source%\msgs-json\*.* %targetc%
 REM Generate the message for Unity Test
 echo #### Generate the message for Unity Test
 set source=msg-cs
-set targetc=..\..\..\support\client-unity\Packages\com.fouridstudio.mizugo-client-unity\Tests\Runtime\Msgs
+set targetc=..\..\support\client-unity\Packages\com.fouridstudio.mizugo-client-unity\Tests\Runtime\Msgs
 
 rm -r %targetc%
 mkdir %targetc%
