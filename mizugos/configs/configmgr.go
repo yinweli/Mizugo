197cd6e6be0c7980079e57d7afbffeff8a069d90
yinweli <yinweli@gmail.com>
2025-08-27 17:35:17 +0800

    Feature | 更新配置管理器

--- mizugos/configs/configmgr.go
+++ mizugos/configs/configmgr.go
@@ -4,7 +4,7 @@ import (
 	"bytes"
 	"fmt"
 	"io"
-	"time"
+	"strings"
 
 	"github.com/spf13/viper"
 )
@@ -16,20 +16,19 @@ func NewConfigmgr() *Configmgr {
 	}
 }
 
-// Configmgr 配置管理器, 內部使用viper實現功能, 有以下幾種讀取配置的模式
-//   - 從檔案讀取配置: 從指定的路徑/檔名/副檔名讀取配置, 需要配合 AddPath 函式設置路徑(可多次設置來指定多個路徑來源)
-//   - 從字串讀取配置: 從字串讀取配置, 由於內部仍然用讀取檔案的方式來處理字串, 所以必須提供來源使用的檔案格式的副檔名
-//   - 從讀取器讀取配置: 從讀取器讀取配置, 由於內部仍然用讀取檔案的方式來處理字串, 所以必須提供來源使用的檔案格式的副檔名
-//   - 從環境變數讀取配置: 從環境變數中讀取配置, 這需要事先決定好前綴字
+// Configmgr 配置管理器, 是一個基於 viper 的配置管理器, 支援多種載入來源:
+//   - 檔案: 透過 AddPath 設定搜尋路徑, 指定檔名與副檔名載入
+//   - 字串: 以字串內容載入, 內部仍走「讀檔流程」, 需提供對應的副檔名
+//   - 讀取器: 自 io.Reader 載入, 同樣需提供副檔名
+//   - 環境變數: 以指定前綴字讀取環境變數
 //
-// 以上讀取配置的模式內部都是用讀取檔案的方式來處理字串, 所以必須提供來源使用的檔案格式的副檔名, 支援的副檔名可以參考 viper.SupportedExts
+// 由於上述載入方式在內部皆以「讀檔模式」處理, 必須提供正確的副檔名, 支援清單可參考 viper.SupportedExts
 //
-// 當配置讀取完畢後, 需要從管理器中取得配置值時, 可以用索引值來呼叫 Get... 系列函式來取得配置值;
-// 或是用索引值來呼叫 Unmarshal 來取得反序列化到結構的配置資料
+// 讀取完成後, 可使用 Unmarshal(key, obj) 將指定節點反序列化為結構體
 //
-// 當同時使用檔案/字串/讀取器以及環境變數時, viper有規定其使用順序, 上面的會覆蓋下面的配置:
+// 多來源同時存在時, viper 的覆蓋優先序如下(上者覆蓋下者):
 //   - 環境變數
-//   - 配置檔案
+//   - 設定檔案
 type Configmgr struct {
 	config *viper.Viper // 配置資料
 	read   bool         // 讀取旗標
@@ -37,7 +36,6 @@ type Configmgr struct {
 
 // Reset 重置配置管理器
 func (this *Configmgr) Reset() {
-	viper.Reset() // 其實這個不執行也沒關係, 但是因為它會重置 viper.SupportedExts 跟 viper.SupportedRemoteProviders, 所以還是跑一下好了
 	this.config = viper.New()
 	this.read = false
 }
@@ -49,8 +47,12 @@ func (this *Configmgr) AddPath(path ...string) {
 	} // for
 }
 
-// ReadFile 從檔案讀取配置, 可以設定多個路徑來源, 支援的副檔名可以參考 viper.SupportedExts
+// ReadFile 從檔案讀取配置, 與 AddPath 搭配使用, 支援的副檔名可以參考 viper.SupportedExts
 func (this *Configmgr) ReadFile(name, ext string) (err error) {
+	if ext == "" {
+		return fmt.Errorf("configmgr readfile: ext empty")
+	} // if
+
 	this.config.SetConfigType(ext)
 	this.config.SetConfigName(name)
 
@@ -62,7 +64,7 @@ func (this *Configmgr) ReadFile(name, ext string) (err error) {
 	} // if
 
 	if err != nil {
-		return fmt.Errorf("configmgr readfile: %v(%v): %w", name, ext, err)
+		return fmt.Errorf("configmgr readfile: %v.%v: %w", name, ext, err)
 	} // if
 
 	return nil
@@ -70,7 +72,11 @@ func (this *Configmgr) ReadFile(name, ext string) (err error) {
 
 // ReadString 從字串讀取配置, 支援的副檔名可以參考 viper.SupportedExts
 func (this *Configmgr) ReadString(input, ext string) (err error) {
-	reader := bytes.NewBuffer([]byte(input))
+	if ext == "" {
+		return fmt.Errorf("configmgr readstring: ext empty")
+	} // if
+
+	reader := bytes.NewBufferString(input)
 	this.config.SetConfigType(ext)
 
 	if this.read == false {
@@ -81,7 +87,7 @@ func (this *Configmgr) ReadString(input, ext string) (err error) {
 	} // if
 
 	if err != nil {
-		return fmt.Errorf("configmgr readstring: %v: %w", ext, err)
+		return fmt.Errorf("configmgr readstring: %w", err)
 	} // if
 
 	return nil
@@ -89,6 +95,10 @@ func (this *Configmgr) ReadString(input, ext string) (err error) {
 
 // ReadBuffer 從讀取器讀取配置, 支援的副檔名可以參考 viper.SupportedExts
 func (this *Configmgr) ReadBuffer(reader io.Reader, ext string) (err error) {
+	if ext == "" {
+		return fmt.Errorf("configmgr readbuffer: ext empty")
+	} // if
+
 	this.config.SetConfigType(ext)
 
 	if this.read == false {
@@ -99,107 +109,37 @@ func (this *Configmgr) ReadBuffer(reader io.Reader, ext string) (err error) {
 	} // if
 
 	if err != nil {
-		return fmt.Errorf("configmgr readbuffer: %v: %w", ext, err)
+		return fmt.Errorf("configmgr readbuffer: %w", err)
 	} // if
 
 	return nil
 }
 
-// ReadEnvironment 從環境變數讀取配置, 讀取時需要指定前綴字;
-// 請注意在整個程式中只使用一個前綴字, 不然就得要在每次取得配置前, 都要執行 ReadEnvironment 來重新指定前綴字;
-// 前綴字與第一層key之間將會以`_`連接, 第一層key與之後的key之間則以`.`連接
-// 以下是個配置檔案與環境變數的映射範例, 假設我們將前綴字設定為 MYAPP
+// ReadEnvironment 從環境變數載入配置, 需指定前綴字; 建議整個程式僅使用單一前綴字, 否則每次取值前都必須重新呼叫此方法設定前綴
+//
+// 命名規則:
+//   - 前綴字與每一層 key 以 `_` 連接
+//   - 環境變數名稱會自動轉為大寫
+//
+// 範例: 若 prefix = "MYAPP", 則以下 YAML 會映射為環境變數:
 //
 //	test:
-//	  value1: a // 環境變數名稱為 MYAPP_TEST.VALUE1
-//	  value2: b // 環境變數名稱為 MYAPP_TEST.VALUE2
-//
-// 在上面的例子中, `test`是第一層key, `value1`與`value2`則是之後的key;
-// 環境變數在讀取時將會轉為全大寫來搜尋
+//	  value1: a // MYAPP_TEST_VALUE1
+//	  value2: b // MYAPP_TEST_VALUE2
 func (this *Configmgr) ReadEnvironment(prefix string) (err error) {
+	if prefix == "" {
+		return fmt.Errorf("configmgr readenvironment: prefix empty")
+	} // if
+
 	this.config.SetEnvPrefix(prefix)
+	this.config.SetEnvKeyReplacer(strings.NewReplacer(".", "_")) // 為了使用方便, 讓每一層 key 的連接都使用 `_`
 	this.config.AutomaticEnv()
 	return nil
 }
 
-// Get 取得配置
-func (this *Configmgr) Get(key string) any {
-	return this.config.Get(key)
-}
-
-// GetBool 取得布林值
-func (this *Configmgr) GetBool(key string) bool {
-	return this.config.GetBool(key)
-}
-
-// GetInt 取得整數
-func (this *Configmgr) GetInt(key string) int {
-	return this.config.GetInt(key)
-}
-
-// GetInt32 取得整數
-func (this *Configmgr) GetInt32(key string) int32 {
-	return this.config.GetInt32(key)
-}
-
-// GetInt64 取得整數
-func (this *Configmgr) GetInt64(key string) int64 {
-	return this.config.GetInt64(key)
-}
-
-// GetUInt 取得整數
-func (this *Configmgr) GetUInt(key string) uint {
-	return this.config.GetUint(key)
-}
-
-// GetUInt32 取得整數
-func (this *Configmgr) GetUInt32(key string) uint32 {
-	return this.config.GetUint32(key)
-}
-
-// GetUInt64 取得整數
-func (this *Configmgr) GetUInt64(key string) uint64 {
-	return this.config.GetUint64(key)
-}
-
-// GetFloat 取得浮點數
-func (this *Configmgr) GetFloat(key string) float64 {
-	return this.config.GetFloat64(key)
-}
-
-// GetString 取得字串
-func (this *Configmgr) GetString(key string) string {
-	return this.config.GetString(key)
-}
-
-// GetIntSlice 取得整數列表
-func (this *Configmgr) GetIntSlice(key string) []int {
-	return this.config.GetIntSlice(key)
-}
-
-// GetStringSlice 取得字串列表
-func (this *Configmgr) GetStringSlice(key string) []string {
-	return this.config.GetStringSlice(key)
-}
-
-// GetTime 取得時間
-func (this *Configmgr) GetTime(key string) time.Time {
-	return this.config.GetTime(key)
-}
-
-// GetDuration 取得時間
-func (this *Configmgr) GetDuration(key string) time.Duration {
-	return this.config.GetDuration(key)
-}
-
-// GetSizeInBytes 取得位元長度
-func (this *Configmgr) GetSizeInBytes(key string) uint {
-	return this.config.GetSizeInBytes(key)
-}
-
 // Unmarshal 反序列化為資料物件
 func (this *Configmgr) Unmarshal(key string, obj any) error {
-	if this.config.InConfig(key) == false {
+	if this.config.IsSet(key) == false {
 		return fmt.Errorf("configmgr unmarshal: %v: not exist", key)
 	} // if
 
@@ -208,4 +148,9 @@ func (this *Configmgr) Unmarshal(key string, obj any) error {
 	} // if
 
 	return nil
+}
+
+// Get 取得配置
+func (this *Configmgr) Get(key string) any {
+	return this.config.Get(key)
 }
--- mizugos/configs/configmgr_test.go
+++ mizugos/configs/configmgr_test.go
@@ -4,9 +4,7 @@ import (
 	"bytes"
 	"os"
 	"testing"
-	"time"
 
-	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/suite"
 
 	"github.com/yinweli/Mizugo/v2/mizugos/trials"
@@ -31,82 +29,65 @@ func (this *SuiteConfigmgr) TearDownSuite() {
 }
 
 func (this *SuiteConfigmgr) TestConfigmgr() {
-	ext := "yaml"
-	name1 := "configmgr"
-	name2 := "!?"
-	value1 := "valid: valid"
-	value2 := "valid=valid"
-	reader1 := bytes.NewBuffer([]byte(value1))
-	reader2 := bytes.NewBuffer([]byte(value2))
-	env := "env"
-	expected := configTester{
-		Value1: 1,
-		Value2: "2",
-		Value3: []string{"a", "b", "c"},
-	}
-	valid := "valid"
 	target := NewConfigmgr()
-	assert.NotNil(this.T(), target)
-	target.AddPath(".")
-	assert.Nil(this.T(), target.ReadFile(name1, ext))
-	assert.Nil(this.T(), target.ReadFile(name1, ext))
-	assert.NotNil(this.T(), target.ReadFile(name2, ext))
-	assert.Equal(this.T(), valid, target.Get(valid))
-	target.Reset()
-
-	target = NewConfigmgr()
-	assert.Nil(this.T(), target.ReadString(value1, ext))
-	assert.Nil(this.T(), target.ReadString(value1, ext))
-	assert.NotNil(this.T(), target.ReadString(value2, ext))
-	assert.Equal(this.T(), valid, target.Get(valid))
-	target.Reset()
-
-	target = NewConfigmgr()
-	assert.Nil(this.T(), target.ReadBuffer(reader1, ext))
-	assert.Nil(this.T(), target.ReadBuffer(reader1, ext))
-	assert.NotNil(this.T(), target.ReadBuffer(reader2, ext))
-	assert.Equal(this.T(), valid, target.Get(valid))
-	target.Reset()
-
-	target = NewConfigmgr()
-	_ = os.Setenv("ENV_VALUE", "abc")
-	assert.Nil(this.T(), target.ReadEnvironment(env))
-	assert.Equal(this.T(), "abc", target.Get("value"))
-	_ = os.Unsetenv("ENV_VALUE")
-	target.Reset()
-
-	target = NewConfigmgr()
-	target.AddPath(".")
-	assert.Nil(this.T(), target.ReadFile(name1, ext))
-	assert.Equal(this.T(), true, target.GetBool("valueb"))
-	assert.Equal(this.T(), 1, target.GetInt("valuei"))
-	assert.Equal(this.T(), int32(100000000), target.GetInt32("valuei32"))
-	assert.Equal(this.T(), int64(100000000000), target.GetInt64("valuei64"))
-	assert.Equal(this.T(), uint(2), target.GetUInt("valueu"))
-	assert.Equal(this.T(), uint32(200000000), target.GetUInt32("valueu32"))
-	assert.Equal(this.T(), uint64(200000000000), target.GetUInt64("valueu64"))
-	assert.Equal(this.T(), 3.33, target.GetFloat("valuef"))
-	assert.Equal(this.T(), "string", target.GetString("values"))
-	assert.Equal(this.T(), []int{1, 2, 3}, target.GetIntSlice("valuelisti"))
-	assert.Equal(this.T(), []string{"a", "b", "c"}, target.GetStringSlice("valuelists"))
-	assert.Equal(this.T(), "2020-12-31 09:30:30", target.GetTime("valuet").Format("2006-01-02 15:04:05"))
-	assert.Equal(this.T(), time.Second*360, target.GetDuration("valued"))
-	assert.Equal(this.T(), uint(1024), target.GetSizeInBytes("valuec"))
-	target.Reset()
-
-	target = NewConfigmgr()
-	actual := configTester{}
-	target.AddPath(".")
-	assert.Nil(this.T(), target.ReadFile(name1, ext))
-	assert.Nil(this.T(), target.Unmarshal("object", &actual))
-	assert.NotNil(this.T(), target.Unmarshal("!?", &actual))
-	assert.NotNil(this.T(), target.Unmarshal("object", nil))
-	assert.Equal(this.T(), expected, actual)
+	this.NotNil(target)
 	target.Reset()
 }
 
-type configTester struct {
-	Value1 int
-	Value2 string
-	Value3 []string
+func (this *SuiteConfigmgr) TestReadFile() {
+	target := NewConfigmgr()
+	target.AddPath(".")
+	this.Nil(target.ReadFile("configmgr", "yaml"))
+	this.Nil(target.ReadFile("configmgr", "yaml"))
+	this.NotNil(target.ReadFile("configmgr", ""))
+	this.NotNil(target.ReadFile(testdata.Unknown, "yaml"))
+	this.Equal(1, target.Get("v1"))
+}
+
+func (this *SuiteConfigmgr) TestReadString() {
+	target := NewConfigmgr()
+	this.Nil(target.ReadString("v1: 1", "yaml"))
+	this.Nil(target.ReadString("v2: 2", "yaml"))
+	this.NotNil(target.ReadString("v2: 2", ""))
+	this.NotNil(target.ReadString(testdata.Unknown, "yaml"))
+	this.Equal(1, target.Get("v1"))
+	this.Equal(2, target.Get("v2"))
+}
+
+func (this *SuiteConfigmgr) TestReadBuffer() {
+	target := NewConfigmgr()
+	this.Nil(target.ReadBuffer(bytes.NewBufferString("v1: 1"), "yaml"))
+	this.Nil(target.ReadBuffer(bytes.NewBufferString("v2: 2"), "yaml"))
+	this.NotNil(target.ReadBuffer(bytes.NewBufferString("v2: 2"), ""))
+	this.NotNil(target.ReadBuffer(bytes.NewBufferString(testdata.Unknown), "yaml"))
+	this.Equal(1, target.Get("v1"))
+	this.Equal(2, target.Get("v2"))
+}
+
+func (this *SuiteConfigmgr) TestReadEnvironment() {
+	_ = os.Setenv("ENV_V1", "1")
+	target := NewConfigmgr()
+	this.Nil(target.ReadEnvironment("env"))
+	this.NotNil(target.ReadEnvironment(""))
+	this.Equal("1", target.Get("v1"))
+	_ = os.Unsetenv("ENV_V1")
+}
+
+func (this *SuiteConfigmgr) TestUnmarshal() {
+	type data struct {
+		V int
+		S []string
+	}
+
+	actual := &data{}
+	target := NewConfigmgr()
+	target.AddPath(".")
+	_ = target.ReadFile("configmgr", "yaml")
+	this.Nil(target.Unmarshal("v2", &actual))
+	this.NotNil(target.Unmarshal(testdata.Unknown, &actual))
+	this.NotNil(target.Unmarshal("v2", nil))
+	this.Equal(&data{
+		V: 2,
+		S: []string{"a", "b", "c"},
+	}, actual)
 }
