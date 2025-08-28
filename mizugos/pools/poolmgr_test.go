a6af6d39d18bc5a93f3bc1f7d7c292711dbf364b
yinweli <yinweli@gmail.com>
2025-08-28 18:35:01 +0800

    Feature | 更新執行緒管理器

--- mizugos/pools/poolmgr_test.go
+++ mizugos/pools/poolmgr_test.go
@@ -5,7 +5,6 @@ import (
 	"sync/atomic"
 	"testing"
 
-	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/suite"
 
 	"github.com/yinweli/Mizugo/v2/mizugos/trials"
@@ -30,25 +29,23 @@ func (this *SuitePoolmgr) TearDownSuite() {
 }
 
 func (this *SuitePoolmgr) TestPoolmgr() {
-	target := NewPoolmgr()
-	assert.NotNil(this.T(), target)
 	config := &Config{
 		Logger: func(format string, args ...any) {
 			fmt.Printf(format, args...)
 			fmt.Printf("\n")
 		},
 	}
-
+	target := NewPoolmgr()
+	this.NotNil(target)
 	target.Finalize() // 初始化前執行, 這次應該不執行
-	assert.Nil(this.T(), target.Initialize(config))
-	assert.NotNil(this.T(), target.Initialize(config)) // 故意啟動兩次, 這次應該失敗
-	assert.NotNil(this.T(), target.Initialize(nil))
+	this.Nil(target.Initialize(config))
+	this.NotNil(target.Initialize(config)) // 故意啟動兩次, 這次應該失敗
+	this.NotNil(target.Initialize(nil))
 	target.Finalize()
 	target.Finalize() // 故意結束兩次, 這次應該不執行
 }
 
 func (this *SuitePoolmgr) TestSubmit() {
-	target := NewPoolmgr()
 	config := &Config{
 		Logger: func(format string, args ...any) {
 			fmt.Printf(format, args...)
@@ -59,50 +56,44 @@ func (this *SuitePoolmgr) TestSubmit() {
 	validFunc := func() {
 		valid.Add(1)
 	}
-
+	target := NewPoolmgr()
 	trials.WaitTimeout()
 	target.Submit(validFunc)
 	trials.WaitTimeout()
-	assert.Equal(this.T(), int64(1), valid.Load())
+	this.Equal(int64(1), valid.Load())
 
-	assert.Nil(this.T(), target.Initialize(config))
+	_ = target.Initialize(config)
 	trials.WaitTimeout()
 	target.Submit(validFunc)
 	trials.WaitTimeout()
 	target.Finalize()
 	trials.WaitTimeout()
-	assert.Equal(this.T(), int64(2), valid.Load())
+	this.Equal(int64(2), valid.Load())
 }
 
 func (this *SuitePoolmgr) TestStatus() {
 	target := NewPoolmgr()
-	assert.Equal(this.T(), Stat{}, target.Status())
+	this.Equal(Stat{}, target.Status())
 	target.Finalize()
 
 	target = NewPoolmgr()
-	assert.NotNil(this.T(), target.Initialize(nil))
-	assert.Equal(this.T(), Stat{}, target.Status())
+	_ = target.Initialize(nil)
+	this.Equal(Stat{}, target.Status())
 	target.Finalize()
 
 	target = NewPoolmgr()
-	assert.Nil(this.T(), target.Initialize(&Config{
+	_ = target.Initialize(&Config{
 		Logger: func(format string, args ...any) {
 			fmt.Printf(format, args...)
 			fmt.Printf("\n")
 		},
-	}))
-	assert.Equal(this.T(), Stat{Available: -1, Capacity: -1}, target.Status())
+	})
+	this.Equal(Stat{Available: -1, Capacity: -1}, target.Status())
 	target.Finalize()
 }
 
-func (this *SuitePoolmgr) TestConfig() {
-	config := Config{}
-	fmt.Println(config)
-	assert.NotNil(this.T(), config.String())
-}
-
-func (this *SuitePoolmgr) TestStat() {
-	stat := Stat{}
-	fmt.Println(stat)
-	assert.NotNil(this.T(), stat.String())
+func (this *SuitePoolmgr) TestLogger() {
+	target := false
+	(&Logger{logger: func(format string, args ...any) { target = true }}).Printf("")
+	this.True(target)
 }
